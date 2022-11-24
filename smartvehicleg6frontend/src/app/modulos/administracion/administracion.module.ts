387341f2def6c1dd91f7ff6eb57eac4a99da40ac
seguraedwin5 <51142779+seguraedwin5@users.noreply.github.com>
2022-11-24 03:16:37 -0500

    solucion crear vehiculo component

--- smartvehicleg6frontend/src/app/modulos/administracion/administracion.module.ts
+++ smartvehicleg6frontend/src/app/modulos/administracion/administracion.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule , ReactiveFormsModule } from '@angular/forms';
 import { CrearPersonaComponent } from './personas/crear-persona/crear-persona.component';
 import { EditarPersonaComponent } from './personas/editar-persona/editar-persona.component';
 import { EliminarPersonaComponent } from './personas/eliminar-persona/eliminar-persona.component';
 import { BuscarPersonaComponent } from './personas/buscar-persona/buscar-persona.component';
-import { CrearVehiculoComponent } from './personas/crear-vehiculo/crear-vehiculo.component';
 import { EditarVehiculoComponent } from './vehiculos/editar-vehiculo/editar-vehiculo.component';
 import { BuscarVehiculoComponent } from './vehiculos/buscar-vehiculo/buscar-vehiculo.component';
 import { EliminarVehiculoComponent } from './vehiculos/eliminar-vehiculo/eliminar-vehiculo.component';
 import { ListarVehiculosComponent } from './vehiculos/listar-vehiculos/listar-vehiculos.component';
 import { AdministracionRoutingModule } from './administracion-routing.module';
+import { CrearVehiculoComponent } from './vehiculos/crear-vehiculo/crear-vehiculo.component';
 
 
 
@@ -28,7 +29,9 @@ import { AdministracionRoutingModule } from './administracion-routing.module';
   ],
   imports: [
     CommonModule,
-    AdministracionRoutingModule
+    AdministracionRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
   ]
 })
 export class AdministracionModule { }
--- smartvehicleg6frontend/src/app/modulos/administracion/vehiculos/crear-vehiculo/crear-vehiculo.component.ts
+++ smartvehicleg6frontend/src/app/modulos/administracion/vehiculos/crear-vehiculo/crear-vehiculo.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { FormBuilder , FormGroup } from '@angular/forms';
+import { ModeloVehiculo } from 'src/app/modelos/vehiculo.modelo';
 @Component({
   selector: 'app-crear-vehiculo',
   templateUrl: './crear-vehiculo.component.html',
   styleUrls: ['./crear-vehiculo.component.css']
 })
-export class CrearVehiculoComponent implements OnInit {
+export class CrearVehiculoComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  
+  constructor(private builder: FormBuilder) { }
+  fgcrear: FormGroup = this.builder.group({
+    'name': []
+  })
 }
