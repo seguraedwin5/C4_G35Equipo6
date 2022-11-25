1928e27215b56cdcd167f645ffcc419e047c6881
Davidfn11 <davidf1_1910@hotmail.com>
2022-11-24 21:53:09 -0500

    InicioSolicitudes

--- smartvehicleg6frontend/src/app/modulos/administracion/administracion.module.ts
+++ smartvehicleg6frontend/src/app/modulos/administracion/administracion.module.ts
@@ -11,6 +11,12 @@ import { EliminarVehiculoComponent } from './vehiculos/eliminar-vehiculo/elimina
 import { ListarVehiculosComponent } from './vehiculos/listar-vehiculos/listar-vehiculos.component';
 import { AdministracionRoutingModule } from './administracion-routing.module';
 import { CrearVehiculoComponent } from './vehiculos/crear-vehiculo/crear-vehiculo.component';
+import { SolicitudesComponent } from './solicitudes/solicitudes.component';
+import { BuscarSolicitudComponent } from './solicitudes/buscar-solicitud/buscar-solicitud.component';
+import { CrearSolicitudComponent } from './solicitudes/crear-solicitud/crear-solicitud.component';
+import { EditarSolicitudComponent } from './solicitudes/editar-solicitud/editar-solicitud.component';
+import { EliminarSolicitudComponent } from './solicitudes/eliminar-solicitud/eliminar-solicitud.component';
+import { ListarSolicitudesComponent } from './solicitudes/listar-solicitudes/listar-solicitudes.component';
 
 
 
@@ -25,6 +31,12 @@ import { CrearVehiculoComponent } from './vehiculos/crear-vehiculo/crear-vehicul
     EditarVehiculoComponent,
     EliminarVehiculoComponent,
     ListarVehiculosComponent,
+    SolicitudesComponent,
+    BuscarSolicitudComponent,
+    CrearSolicitudComponent,
+    EditarSolicitudComponent,
+    EliminarSolicitudComponent,
+    ListarSolicitudesComponent,
     
   ],
   imports: [
---
+++ smartvehicleg6frontend/src/app/modulos/administracion/solicitudes/listar-solicitudes/listar-solicitudes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-solicitudes',
+  templateUrl: './listar-solicitudes.component.html',
+  styleUrls: ['./listar-solicitudes.component.css']
+})
+export class ListarSolicitudesComponent {
+
+}
